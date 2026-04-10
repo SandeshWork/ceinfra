f863796ce267c2fb2fbf681c0f82363f3111f6c9
Sandesh MSI <work@sandeshy.com>
2026-04-10 18:25:25 +0530

    Fix favicon implementation - use simple HTML link reference

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,6 @@
   import { createRoot } from "react-dom/client";
   import App from "./app/App.tsx";
   import "./styles/index.css";
-  import faviconUrl from "./assets/ce-logo.png";
-
-  // Set favicon dynamically
-  const link = document.querySelector("link[rel='icon']") || document.createElement("link");
-  link.rel = "icon";
-  link.type = "image/png";
-  link.href = faviconUrl;
-  if (!document.querySelector("link[rel='icon']")) {
-    document.head.appendChild(link);
-  }
 
   createRoot(document.getElementById("root")!).render(<App />);
   
